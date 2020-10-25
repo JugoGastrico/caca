aaa314e1f828415092fb188c6efde0eaecb75b2d
Alexander <alexandertisko@hotmail.com>
2020-10-25 18:50:42 -0300

    modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,11 +2,13 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Module'
 
 
 let mobileMenu = new MobileMenu();
 let stickyHeader = new StickyHeader();
 
+new Modal ()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealOnScroll(document.querySelectorAll(".testiomial"), 60);
 
